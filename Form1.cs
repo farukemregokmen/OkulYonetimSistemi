35086ae50b5c94dde5a43f651922fda1c7fef918
Faruk Emre Gokmen <emreemre200412@gmail.com>
2025-10-01 23:36:16 +0300

    Project and database created.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace OkulYonetimSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+    }
+}
